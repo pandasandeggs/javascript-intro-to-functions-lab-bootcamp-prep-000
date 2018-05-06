589a2b3b216b79f5d5cd93cb271dc0c96b5192ca
Chantal Wallace <theteatreehouse@gmail.com>
2018-05-06 04:43:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,13 +11,14 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string){
-  if string === string.ToLowerCase
+  if (string === string.ToLowerCase)
   return "I can't hear you!"
 }
 function sayHiToGrandma(string){
-  if string===string.toUpperCase
+  if (string===string.toUpperCase)
   return `"YES INDEED!"`
 }
 function sayHiToGrandma(string){
-  if string==="I love you, Grandma."
+  if (string==="I love you, Grandma.")
   return `"I love you, too."`
+}
